708f079c4b70967438572c86b2ff9fc7203b9802
Dmitry Santalov <dmitsantalov@yandex.ru>
2024-09-16 10:46:59 +0300

    Update Parallel.java

--- src/Parallel.java
+++ src/Parallel.java
@@ -2,14 +2,10 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class Parallel implements Runnable {
-
     @Override
     public void run() {
-
         System.out.println("Hello from " + Thread.currentThread().getName());
-
     }
-
 }
 
 
