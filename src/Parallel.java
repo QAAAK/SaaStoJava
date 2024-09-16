354ec385cdedd4751fe12c1115f81f2476759e64
Dmitry Santalov <dmitsantalov@yandex.ru>
2024-09-16 10:46:38 +0300

    Update Parallel.java

--- src/Parallel.java
+++ src/Parallel.java
@@ -12,21 +12,4 @@ public class Parallel implements Runnable {
 
 }
 
-//class Main {
-//
-//    public static void main (String[] args) {
-//
-//        ExecutorService executor = Executors.newFixedThreadPool(10); //quantity thread
-//        executor.submit(new Thread(new Parallel()));
-//
-//
-//
-//        executor.shutdown();
-////        for (int i=0; i<100; i++) {
-////
-//////            Thread th = new Thread(new Parallel());
-//////            th.start();
-////        }
-//    }
-//
-//}
+
